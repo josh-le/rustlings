cf8b938cc297a89d67e5a1292c3a81088dff2940
Verified ✔️ <joshcapitals07@gmail.com>
2025-07-23 18:13:06 -0400

    16_3 done, situation where we had to define the lifetime on a struct because it was using string splits

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -1,10 +1,15 @@
 // Lifetimes are also needed when structs hold references.
 
 // TODO: Fix the compiler errors about the struct.
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
+
+// struct Book {
+//     author: &str,
+//     title: &str,
+// }
 
 fn main() {
     let book = Book {
